9589154edc93fc0000ef9cff9554d9dd00d3eb39
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-12 13:13:01 +0200

    Search bar fix

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760262455',resources: [
+    cacheName: 'chirpy-1760267212',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
