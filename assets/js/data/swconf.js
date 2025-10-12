fc7a29875d64da350fcbe8171aaec95cc9e5f79d
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-12 13:20:23 +0200

    Administrator name fix

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760267212',resources: [
+    cacheName: 'chirpy-1760267978',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
