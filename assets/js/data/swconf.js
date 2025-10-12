073a2a44ba23ac4c2e7774e7ae899c1f91933a48
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-12 11:49:58 +0200

    Administrator Release

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749731076',resources: [
+    cacheName: 'chirpy-1760262455',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
