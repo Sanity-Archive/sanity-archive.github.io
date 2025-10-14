6d893b412524dfa78d246cefd2bc34d8168acc50
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-14 14:49:14 +0200

    Redelegate Release

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760346533',resources: [
+    cacheName: 'chirpy-1760446065',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
