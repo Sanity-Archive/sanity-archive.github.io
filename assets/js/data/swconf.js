1bcdf4025f0522a5b7e76bf136cfd4320efcf714
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-16 13:46:24 +0200

    Builder release and Interactive shell update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760446065',resources: [
+    cacheName: 'chirpy-1760615083',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
