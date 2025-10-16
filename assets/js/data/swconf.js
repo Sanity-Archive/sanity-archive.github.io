c3f0c372e9c09d83043fa84c664e00dab1ef7064
Sanity-Archive <lara.odenkirchen@outlook.com>
2025-10-16 13:56:34 +0200

    Builder visual fix decryption

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1760615083',resources: [
+    cacheName: 'chirpy-1760615634',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
